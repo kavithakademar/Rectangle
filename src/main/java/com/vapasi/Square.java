870ea60c50e910a89c363296a6706d71d9df541c
ramyaganesan.ragu <ramyaganesan.ragu@WORKSPACES.THOUGHTWORKS.COM>
2020-11-24 15:15:20 +0530

    [Ramya/Kavitha] Create square class

---
+++ src/main/java/com/vapasi/Square.java
@@ -0,0 +1,16 @@
+package com.vapasi;
+
+public class Square {
+
+    private int side;
+
+    public Square(int side) {
+        this.side = side;
+    }
+
+    public int area()
+    {
+        return 9;
+
+    }
+}
